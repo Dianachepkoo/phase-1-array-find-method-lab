ec98fcf5eed551bb054f12dd6a55bb84e69127a2
Diana Chepkoech <dianachepkoech336@gmail.com>
2023-10-11 10:18:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+
+function superbowlWin(record) {
+    const winEntry = record.find(entry => entry.result === 'W');
+  
+
+    return winEntry ? winEntry.year : undefined;
+  }
+  
